3f7a043abc122db8e6ccf1c1ad4003e1e3492099
Carl J Nash <carl@carljnash.com>
2025-04-14 16:25:55 +0100

    Initial Commit

---
+++ TestDebouncer/TestDebouncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDebouncerApp.swift
+//  TestDebouncer
+//
+//  Created by Carl Nash on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestDebouncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
